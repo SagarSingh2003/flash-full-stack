91c875fbf826a3333b78532171b2c206c86471e8
diptimalick <imdiptilal@gmail.com>
2024-09-22 13:03:05 +0530

    old backend

--- flash-backend/controller/cardController.js
+++ flash-backend/controller/cardController.js
@@ -1,50 +1,23 @@
 import pool from "../model/db.js";
 import queryBuilder from "../utils/queryBuilder.js";
-import 'dotenv/config';
-import { GoogleGenerativeAI } from "@google/generative-ai";
 
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.AI_API_KEY);
-            
 const cardController = {
     getAllCards : async (req , res) => {
         try{
             
-            const deck_id  = req.query.deck_id;
-            let rows = [];
-
-            if(deck_id){
-                rows = await pool.query(`select * from flashcard WHERE user_id='all' OR deck_id='${deck_id}'`);
-            }
-            
-            console.log(rows[0]);
-            // console.log(rows , "rows");
-            res.status(200).json({    
-                cards : rows[0]
-            });   
-            
-        }catch(err){
-            console.log(err);
-            res.status(500).json({
-                msg : err
-            })
-        }     
-    },
-    getAllDecks : async (req , res) => {
-        try{
-            
-            const user_id  = req.params.user_id;
-            console.log(user_id , "user_id");
+            const user_id  = req.query.user_id;
             let rows = [];
 
             if(user_id){
-                rows = await pool.query(`select * from Decks WHERE user_id='${user_id}'`);
+                rows = await pool.query(`select * from flashcard WHERE user_id='all' OR user_id='${user_id}'`);
+            }else{
+                rows = await pool.query("select * from flashcard WHERE user_id='all'");
+                console.log(rows);
             }
-            
-            console.log(rows[0]);
-            // console.log(rows , "rows");
+
+            console.log(rows , "rows");
             res.status(200).json({    
-                decks : rows[0]
+                cards : rows[0]
             });   
             
         }catch(err){
@@ -61,7 +34,7 @@ const cardController = {
 
             const [rows]= await pool.query(`select * from flashcard WHERE user_id='${user_id}'`);
 
-            // console.log(rows , "rows");
+            console.log(rows , "rows");
             res.status(200).json({    
                 cards : rows
             });   
@@ -73,32 +46,11 @@ const cardController = {
             })
         }     
     },
-    createDeck : async (req ,res) => {
-        try {
-            console.log(req.body.deck);
-            const {title , user_id} = JSON.parse(req.body.deck);
-            const [rows] = await pool.query(`INSERT INTO Decks(title , user_id) VALUES("${String(title)}" , "${String(user_id)}");`)
-
-            console.log('got create request');
-            res.status(200).json({
-                msg : "deck created successfully",
-                successful : "true"
-            });
-
-        }catch(err){
-            
-            console.log(err);
-            res.status(500).json({
-                msg : err,
-                successful : false
-            })
-        }
-    },
     createCard : async (req ,res) => {
         try {
             
-            const {question , answer , category , user_id , deck_id} = JSON.parse(req.body.card);
-            const [rows] = await pool.query(`INSERT INTO flashcard(question , answer , category , user_id , deck_id) VALUES("${String(question)}" , "${String(answer)}" , "${String(category)}" , "${String(user_id)}" , "${String(deck_id)}");`)
+            const {question , answer , category , user_id} = JSON.parse(req.body.card);
+            const [rows] = await pool.query(`INSERT INTO flashcard(question , answer , category , user_id) VALUES("${String(question)}" , "${String(answer)}" , "${String(category)}" , "${String(user_id)}");`)
 
             console.log(req.body.card.question);
             console.log('got create request');
@@ -124,8 +76,8 @@ const cardController = {
 
             for(let i = 0 ; i < arrOfCardInfo.length ; i++){
     
-                const {question , answer , category , user_id , deck_id} = arrOfCardInfo[i];
-                const [rows] = await pool.query(`INSERT INTO flashcard(question , answer , category , user_id , deck_id) VALUES("${String(question)}" , "${String(answer)}" , "${String(category)}" , "${String(user_id)}" , "${String(deck_id)}");`)
+                const {question , answer , category , user_id} = arrOfCardInfo[i];
+                const [rows] = await pool.query(`INSERT INTO flashcard(question , answer , category , user_id) VALUES("${String(question)}" , "${String(answer)}" , "${String(category)}" , "${String(user_id)}");`)
                 console.log(rows , "rows data");
             }
 
@@ -189,73 +141,8 @@ const cardController = {
                 successful : false
             })
         }
-    },
-    deleteAllCards : async(req , res) => {
-        try {
-
-
-            const [rows] = await pool.query(`DELETE FROM flashcard`)
-
-            res.status(200).json({
-                msg : "successful",
-                successful : 'true'
-            })
-        }catch(err){
-            console.log(err);
-            res.status(500).json({
-                msg : `${err}`,
-                successful : false
-            })
-        }
-    },
-    aiGeneratedCards : async(req , res) => {
-        try{
-            const {user_id ,deck_id, paragraph} = req.body;
-
-            console.log(user_id , paragraph , req.body);
-            let model = genAI.getGenerativeModel({
-                model: "gemini-1.5-flash",
-                // Set the `responseMimeType` to output JSON
-                generationConfig: { responseMimeType: "application/json" }
-              });
-              
-              let prompt = `
-              form question answers that you feel are important also assign a category to them which is just a measure of how difficult or how complex the question is , the category has 3 values which are beginner , intermediate and advance from the given paragraph   :  ${paragraph} also insert user_id as : ${user_id} and deck_id as : ${deck_id} and return an array of objects using this JSON schema:
-              { "type": "object",
-                "properties": {
-                  "question": { "type": "string" },
-                  "answer": {"type": "string"},
-                  "category":{"type":"string"},
-                  "user_id" : {"type" : "string"},
-                  "deck_id" : {"type" : "string"}
-                }
-              }`;
-              
-              let result = await model.generateContent(prompt)
-              const stringResult = JSON.stringify(result.response.text());
-              const jsonData = JSON.parse(stringResult);
-              for(let i = 0 ; i < JSON.parse(jsonData).length ; i++){
-    
-                const {question , answer , category , user_id , deck_id} = JSON.parse(jsonData)[i];
-                const [rows] = await pool.query(`INSERT INTO flashcard(question , answer , category , user_id , deck_id) VALUES("${String(question)}" , "${String(answer)}" , "${String(category)}" , "${String(user_id)}" , "${String(deck_id)}")`)
-                console.log(rows , "rows data");
-            }
-              
-                res.status(200).json({
-                    msg : "successful",
-                    successful : 'true'
-                })
-              
-        }catch(err){
-            console.log(err);
-            res.status(500).json({
-                msg : `${err}`,
-                successful : false
-            })
-        }
     }
 }
-
 
 
 export default cardController;
